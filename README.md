a09c2d69549ba60d03679565692521aab23662be
Chengings <chengings@gmail.com>
2021-01-17 21:28:54 +0000

    - Remove unused prefs. - Add sample of `ln` script to README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # firefox-user.js
+
+Inspiration: https://github.com/pyllyukko/user.js and https://github.com/arkenfox/user.js
+
+macOS or linux can use sym link (ln -s); to get user.js inside profile folder
+```sh
+cd <target profile>
+ln -s ~/firefox-user.js/user.js user.js
+```
--- user.js
+++ user.js
@@ -1,26 +1,11 @@
-// inspiration: https://github.com/pyllyukko/user.js and https://github.com/ghacksuserjs/ghacks-user.js
-// macOS or linux can use sym link (ln -s); to get user.js inside profile folder
 
-// disable webide
-user_pref("devtools.webide.enabled", false);
-user_pref("devtools.webide.autoinstallADBHelper", false);
-user_pref("devtools.webide.autoinstallFxdtAdapters", false);
 
-// disable remote debugging
-user_pref("devtools.debugger.remote-enabled", false);
-user_pref("devtools.chrome.enabled", false);
-user_pref("devtools.debugger.force-local", true);
-
-// cahce dns
-user_pref("network.dnsCacheExpiration", 600);
-user_pref("network.dnsCacheExpirationGracePeriod", 600);
+// cache dns
+user_pref("network.dnsCacheExpiration", 300);
+user_pref("network.dnsCacheExpirationGracePeriod", 300);
 
 // add more connections to server
 user_pref("network.http.max-persistent-connections-per-server", 20);
-
-// no java and flash
-user_pref("plugin.state.flash", 0);
-user_pref("plugin.state.java", 0);
 
 // reader mode
 user_pref("reader.color_scheme", "sepia");
@@ -39,8 +24,7 @@ user_pref("services.sync.scheduler.immediateInterval", 5);
 user_pref("browser.cache.memory.max_entry_size", -1);
 
 // media
-user_pref("media.memory_cache_max_size", 2048000);
+// user_pref("media.memory_cache_ max_size", 2048000);
 user_pref("media.memory_caches_combined_limit_kb", 1000000);
 
 user_pref("security.secure_connection_icon_color_gray", false);
-
